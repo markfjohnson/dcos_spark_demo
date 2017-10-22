207eadaefa02c8d9a8e2979d45288013cdfa2c42
markjohnson <markfjohnson@gmail.com>
2017-10-22 17:00:59 -0400

    Updated project

--- kafka_spark/kafka_producer_example.py
+++ kafka_spark/kafka_producer_example.py
@@ -4,21 +4,26 @@ from kafka import KafkaProducer
 import time
 
 kafka_url = "broker.kafka.l4lb.thisdcos.directory:9092"
+#kafka_url = "localhost:9092"
 topic_name = "example_topic"
 
-def acked(err, msg):
-    if err is not None:
-        print("Failed to deliver message: {0}: {1}"
-              .format(msg.value(), err.str()))
-    else:
-        print("Message produced: {0}".format(msg.value()))
+# def acked(err, msg):
+#     if err is not None:
+#         print("Failed to deliver message: {0}: {1}"
+#               .format(msg.value(), err.str()))
+#     else:
+#         print("Message produced: {0}".format(msg.value()))
+#         print("Message produced: {0}".format(msg.value()))
+
 
 p = KafkaProducer(bootstrap_servers=kafka_url)
 
 iter_count = 0
 
 while True:
-    p.send(topic_name, b"test message - {}".format(iter_count))
+    msg = b"test message - {}".format(iter_count)
+    print msg
+    p.send(topic_name, msg)
     time.sleep(1)
 
 p.flush(30)
