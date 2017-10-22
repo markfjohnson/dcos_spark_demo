60b3e9bfa4cf4b5861477a81e5c5b13478a10d6b
markjohnson <markfjohnson@gmail.com>
2017-10-22 13:03:32 -0400

    Added Cassandra RW Example

---
+++ cassandra_spark/Cassandra_rw.py
@@ -0,0 +1,30 @@
+rom cassandra.cluster import Cluster
+from cassandra import ConsistencyLevel
+from cassandra.query import *
+
+from pyspark import SparkContext, SparkConf, SQLContext
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+from pyspark.sql import SQLContext
+
+KEYSPACE="dcos_example"
+TABLE_NAME="sample_table"
+
+sc = SparkContext(appName="PySpark Cassandra File Write Example")
+
+spark = SparkSession.builder \
+    .appName("PySpark Cassandra Read Write Example") \
+    .config("spark.sql.crossJoin.enabled", "true") \
+    .getOrCreate()
+
+
+cluster = Cluster(['node-0.cassandra.mesos'])  # provide contact points and port
+
+# Verify Tables and Keyspaces exist for data if not then create it
+session = cluster.connect()
+
+session.execute("CREATE KEYSPACE {} IF NOT EXISTS WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '2' }".format( KEYSPACE))
+
+session.execute("USE {};".format(KEYSPACE))
+session.execute("CREATE TABLE IF NOT EXISTS {} ( user_id timeuuid PRIMARY KEY, added_date timestamp, first_name text, last_name text, email text);".format(TABLE_NAME))
+session.execute("DESCRIBE TABLES;")
