a807c148f91c7f6b65944d1395c702410f9ff423
markjohnson <markfjohnson@gmail.com>
2017-10-22 19:34:34 -0400

    kafka broker connection update

--- kafka_spark/kafka_consumer_example.py
+++ kafka_spark/kafka_consumer_example.py
@@ -1,20 +1,17 @@
 from kafka import KafkaConsumer
 
 print("Start of Kafka consumer program")
-
-#kafka_url = "localhost:9092"
 #kafka_url = "broker.kafka.l4lb.thisdcos.directory:9092"
-kafka_url = "broker-0.kafka.mesos:9843"
+kafka_url = "localhost:9092"
 
 topic_name = "example_topic"
 
 print("Connecting to {}".format(kafka_url))
-consumer = KafkaConsumer(bootstrap_servers=kafka_url,
+consumer = KafkaConsumer(bootstrap_servers=kafka_url,api_version=(0,10),
                         auto_offset_reset='earliest')
 print("Connection complete")
 consumer.subscribe(topic_name)
 print("Subscribed to topic {}".format(topic_name))
-
 for message in consumer:
     print (message)
 
--- kafka_spark/kafka_producer_example.py
+++ kafka_spark/kafka_producer_example.py
@@ -4,20 +4,10 @@ from kafka import KafkaProducer
 import time
 
 kafka_url = "broker.kafka.l4lb.thisdcos.directory:9092"
-kafka_url = "broker-0.kafka.mesos:9843"
 #kafka_url = "localhost:9092"
 topic_name = "example_topic"
 
-# def acked(err, msg):
-#     if err is not None:
-#         print("Failed to deliver message: {0}: {1}"
-#               .format(msg.value(), err.str()))
-#     else:
-#         print("Message produced: {0}".format(msg.value()))
-#         print("Message produced: {0}".format(msg.value()))
-
-
-p = KafkaProducer(bootstrap_servers=kafka_url)
+p = KafkaProducer(bootstrap_servers=kafka_url,api_version=(0,10))
 
 iter_count = 0
 
