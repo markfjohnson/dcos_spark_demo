d0cac45bbc94523623dd8dc272f8274560a8eb31
markjohnson <markfjohnson@gmail.com>
2017-10-22 17:12:02 -0400

    Updated project

--- kafka_spark/kafka_consumer_example.py
+++ kafka_spark/kafka_consumer_example.py
@@ -6,12 +6,15 @@ kafka_url = "broker.kafka.l4lb.thisdcos.directory:9092"
 kafka_url = "broker-0.kafka.mesos:9843"
 topic_name = "example_topic"
 
-
+print("Connecting to {}".format(kafka_url))
 consumer = KafkaConsumer(bootstrap_servers=kafka_url,
                         auto_offset_reset='earliest')
+print("Connection complete")
 consumer.subscribe(topic_name)
+print("Subscribed to topic {}".format(topic_name))
 
 for message in consumer:
     print (message)
 
 consumer.close()
+print("Completed reading kafka topic")
