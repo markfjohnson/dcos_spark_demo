4bd333cf4e8ca29d3a3e92b382113c3a21e9679c
markjohnson <markfjohnson@gmail.com>
2017-10-22 22:52:17 -0400

    Updated kafka examples

--- kafka_spark/kafka_consumer_example.py
+++ kafka_spark/kafka_consumer_example.py
@@ -7,13 +7,16 @@ kafka_url = "localhost:9092"
 topic_name = "example_topic"
 
 print("Connecting to {}".format(kafka_url))
-consumer = KafkaConsumer(bootstrap_servers=kafka_url,api_version=(0,10),
-                        auto_offset_reset='earliest')
-print("Connection complete")
-consumer.subscribe(topic_name)
-print("Subscribed to topic {}".format(topic_name))
+consumer = KafkaConsumer(topic_name, bootstrap_servers=kafka_url,
+                         group_id="PrimaryGroup",
+                         enable_auto_commit=True,
+                         api_version=(0,10))
+print ('Start consuming')
 for message in consumer:
-    print (message)
-
-consumer.close()
-print("Completed reading kafka topic")
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print(message)
+#    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+#                                          message.offset, message.key,
+#                                          message.value))
+print("Done consuming")
--- kafka_spark/kafka_producer_example.py
+++ kafka_spark/kafka_producer_example.py
@@ -3,8 +3,9 @@
 from kafka import KafkaProducer
 import time
 
-kafka_url = "broker.kafka.l4lb.thisdcos.directory:9092"
+#kafka_url = "broker.kafka.l4lb.thisdcos.directory:9092"
 #kafka_url = "localhost:9092"
+kafka_url = ["kafka-0-broker.kafka.autoip.dcos.thisdcos.directory:1025","kafka-1-broker.kafka.autoip.dcos.thisdcos.directory:1025", "kafka-2-broker.kafka.autoip.dcos.thisdcos.directory:1025"]
 topic_name = "example_topic"
 
 p = KafkaProducer(bootstrap_servers=kafka_url,api_version=(0,10))
@@ -18,4 +19,3 @@ while True:
     time.sleep(1)
     iter_count = iter_count + 1
 
-p.flush(30)
