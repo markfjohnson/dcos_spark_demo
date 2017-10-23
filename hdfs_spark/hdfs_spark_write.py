6269cb5f50527c7adf2dc212ee7c9f145fe9738e
markjohnson <markfjohnson@gmail.com>
2017-10-23 00:25:30 -0400

    Fixed sparkcontext

--- hdfs_spark/hdfs_spark_write.py
+++ hdfs_spark/hdfs_spark_write.py
@@ -4,6 +4,6 @@ from pyspark.sql import SparkSession
 warehouseLocation="/"
 spark = SparkSession.builder.appName("SparkSessionHDFSExample").config("spark.sql.warehouse.dir", warehouseLocation).enableHiveSupport().getOrCreate()
 
-sc = spark.sparkContext()
+sc = spark.sparkContext
 values = sc.parallelize(range(1,1000))
 spark.saveAsTextFile("/testfile")
