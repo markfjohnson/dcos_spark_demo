4bb8492cf1e6a8027162af30eb12b5e20328648e
markjohnson <markfjohnson@gmail.com>
2017-10-23 00:21:08 -0400

    Changed builder()

--- hdfs_spark/hdfs_spark_write.py
+++ hdfs_spark/hdfs_spark_write.py
@@ -2,7 +2,7 @@ from pyspark import SparkContext, SparkConf, SQLContext
 from pyspark.sql import SparkSession
 
 warehouseLocation="/"
-spark = SparkSession.builder().appName("SparkSessionHDFSExample").config("spark.sql.warehouse.dir", warehouseLocation).enableHiveSupport().getOrCreate()
+spark = SparkSession.builder.appName("SparkSessionHDFSExample").config("spark.sql.warehouse.dir", warehouseLocation).enableHiveSupport().getOrCreate()
 
 sc = spark.sparkContext()
 values = sc.parallelize(range(1,1000))
