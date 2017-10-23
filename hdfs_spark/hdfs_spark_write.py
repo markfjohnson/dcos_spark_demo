df03c5f0e183e21fdd08b3d76c051d177413cbe2
markjohnson <markfjohnson@gmail.com>
2017-10-22 23:42:26 -0400

    Post initial HDFS version

---
+++ hdfs_spark/hdfs_spark_write.py
@@ -0,0 +1,9 @@
+from pyspark import SparkContext, SparkConf, SQLContext
+from pyspark.sql import SparkSession
+
+warehouseLocation="/"
+spark = SparkSession.builder().appName("SparkSessionHDFSExample").config("spark.sql.warehouse.dir", warehouseLocation).enableHiveSupport().getOrCreate()
+
+sc = spark.sparkContext()
+values = sc.parallelize(range(1,1000))
+spark.saveAsTextFile("/testfile")
