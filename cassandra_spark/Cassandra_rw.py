42d21bc5f59a72cedd997b1cad2ae70d17e8b37f
markjohnson <markfjohnson@gmail.com>
2017-10-22 22:12:33 -0400

    Elastic test program pointing to DC/OS std connection

--- cassandra_spark/Cassandra_rw.py
+++ cassandra_spark/Cassandra_rw.py
@@ -1,16 +1,9 @@
 from cassandra.cluster import Cluster
-from cassandra import ConsistencyLevel
-from cassandra.query import *
-
 from pyspark import SparkContext, SparkConf, SQLContext
 from pyspark.sql import SparkSession
-from pyspark import SparkConf
-from pyspark.sql import SQLContext
 
-cassandra_url = 'node-0.cassandra.mesos'
+cassandra_url = 'node.cassandra.l4lb.thisdcos.directory'
 #cassandra_url = 'localhost'
-KEYSPACE="dcos_example"
-TABLE_NAME="sample_table"
 
 sc = SparkContext(appName="PySpark Cassandra File Write Example")
 
@@ -20,13 +13,18 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 
-cluster = Cluster([cassandra_url])  # provide contact points and port
-
-# Verify Tables and Keyspaces exist for data if not then create it
+cluster = Cluster([cassandra_url])
 session = cluster.connect()
 
-session.execute("CREATE KEYSPACE IF NOT EXISTS {} WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'datacenter1' : 3 }; ".format(KEYSPACE))
+session.execute("""CREATE KEYSPACE IF NOT EXISTS dcos_example WITH 
+    REPLICATION = { \'class\' : 'NetworkTopologyStrategy', 'datacenter1' : 3 }; """)
 
-session.execute("USE {};".format(KEYSPACE))
-session.execute("CREATE TABLE IF NOT EXISTS {} ( user_id timeuuid PRIMARY KEY, added_date timestamp, first_name text, last_name text, email text);".format(TABLE_NAME))
-session.execute("DESCRIBE TABLES;")
+session.execute("USE dcos_example;")
+session.execute("CREATE TABLE IF NOT EXISTS sample_table ( user_id timeuuid PRIMARY KEY, added_date timestamp, first_name text, last_name text, email text);")
+ex_keys = cluster.metadata.keyspaces
+for key in ex_keys:
+    tables = ex_keys[key].tables
+    for t in tables:
+        print key, t
+    print("------")
+print "DONE"
